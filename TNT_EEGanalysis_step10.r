9a23d2cb2585cd1e8646897ceeef2c2620ecc8b4
Adam Baker <bakerdon@musc.edu>
2025-11-11 15:55:58 -0500

    cleaned up r script

--- TNT_EEGanalysis_step10.r
+++ TNT_EEGanalysis_step10.r
@@ -9,15 +9,15 @@ library(readr) # for reading csv files
 library(dplyr) # for data manipulation
 library(ggplot2) # for visualization
 library(tidyr) # for data tidying
-
+library(Hmisc)
 # import CSV file with subject IDs and analyze CMC over time, as well as beta vs gamma, muscle, brain region, etc
 # set working directory to folder with all step9 excel files
-cmc_data <- read.csv("/Users/DOB223/Library/CloudStorage/OneDrive-MedicalUniversityofSouthCarolina/Documents/lab/studies/1eeg/TNTanalysis/cmcDataTNT.csv")
+cmcData.df <- read_csv("/Users/DOB223/Library/CloudStorage/OneDrive-MedicalUniversityofSouthCarolina/Documents/lab/studies/1eeg/TNTanalysis/allDataForAnalyze.txt")
 
 ## sanity check to assess if the import in matlab was done correctly
 
 # first, check how many rows there are per subject
-summary_by_subject <- cmc_data %>%
+summary_by_subject <- cmcData.df %>%
     group_by(Subject) %>%
     dplyr::summarise(n_rows = n()) %>%
     arrange(n_rows)
@@ -31,84 +31,25 @@ ggplot(summary_by_subject, aes(x = Subject, y = n_rows)) +
         y = "Number of Rows"
     )
 
-
-# ---------------------- # ---------------------- #
-# ------------- add some motor data ------------- #
-# ---------------------- # ---------------------- #
-
-# load in csv data with demographics & motor data
-motor_demog_data <- read_csv("/Users/DOB223/Library/CloudStorage/OneDrive-MedicalUniversityofSouthCarolina/Documents/lab/studies/1eeg/TNTanalysis/dataForStats/clean_covariates_2025-09-15.csv",
-    na = c("Invalid Number", "invalid number", "INVALID NUMBER", "blank", "Blank", "BLANK", "")
-)
-
-# remove pre-post and pre-fu variables before reshaping the data
-motor_demog_data <- motor_demog_data %>%
-    select(-matches("PRE-POST|PRE-FU", ignore.case = TRUE))
-
-# reshape to long format
-motor_demog_data_long <- motor_demog_data %>%
-    pivot_longer(
-        cols = -c(Subject, group, Sex, `TSS (month)`, `age (yr)`, race, `affected UE impacted?`),
-        names_to = "metric",
-        values_to = "value"
-    )
-
-# add new column descirbing timePoint for matching to cmc_data
-motor_demog_data_long <- motor_demog_data_long %>%
-    mutate(
-        timePoint = case_when(
-            grepl("pre", metric, ignore.case = TRUE) ~ "Pre",
-            grepl("post", metric, ignore.case = TRUE) ~ "Post",
-            grepl("FU", metric, ignore.case = TRUE) ~ "FU",
-            TRUE ~ NA_character_
-        ),
-        metric = gsub("_Pre|_Post|_FU", "", metric, ignore.case = TRUE)
-    )
-
-# testing looks good. Now merge the full datasets
-cmc_data_full <- cmc_data %>%
-    left_join(
-        motor_demog_data,
-        by = c("Subject")
-    )
-
-# clean up 
-rm(cmc_data, motor_demog_data, motor_demog_data_long,summary_by_subject)
-
-
 ################################################
 ########### descriptive statistics #############
 ################################################
 
 # basic descriptives of cmc_data_full
-summary(cmc_data_full)
-describe(cmc_data_full)
+summary(cmcData.df)
+describe(cmcData.df)
 
 # check for missing values
-missing_values <- sapply(cmc_data_full, function(x) sum(is.na(x)))
+missing_values <- sapply(cmcData.df, function(x) sum(is.na(x)))
 print(missing_values)
 
-# histogram of age and various other demographics
-ggplot(cmc_data_full, aes(x = `age (yr)`)) + 
-  geom_histogram()
-
-# WMFT PRE overall (not hand only)
-ggplot(distinct(cmc_data_full, Subject, .keep_all = TRUE), 
-       aes(x = `WMFT PRE avg time`)) + 
+# WMFT
+ggplot(distinct(cmcData.df, Subject, .keep_all = TRUE), 
+       aes(x = `WMFTavgHand`)) + 
   geom_histogram(binwidth = 10) +
   labs(
     title = "Distribution of WMFT PRE Average Hand Scores",
     x = "WMFT PRE Average Hand Score",
-    y = "Number of Subjects"
-  )
-
-# FMA UE PRE 
-ggplot(distinct(cmc_data_full, Subject, .keep_all = TRUE), 
-       aes(x = `FMA PRE total /66`)) + 
-  geom_histogram(binwidth = 6) +
-  labs(
-    title = "Distribution of FMA PRE scores /66",
-    x = "FMA PRE /66",
     y = "Number of Subjects"
   )
 
